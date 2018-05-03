5c58dd9ccb68aaf96bdc2c9c898eb54c318d0f02
student <student@M15018>
2018-05-03 17:39:49 +0900

    방번호 문제 수정

--- AlgorithmExamClass/src/step2/방번호.java
+++ AlgorithmExamClass/src/step2/방번호.java
@@ -11,7 +11,7 @@ public class 방번호 {
 		Scanner scan = new Scanner(System.in);
 		int roomNumber = scan.nextInt();
 		scan.nextLine();
-
+                                                                                                                    
 		String roomStr = String.valueOf(roomNumber);
 		int size = roomStr.length();
 		int[] numbers = new int[10];
@@ -20,11 +20,17 @@ public class 방번호 {
 			numbers[Character.getNumericValue(roomStr.charAt(i))]++;
 		}
 		
-		if ((numbers[6] + numbers[9]) % 2 == 0) {
-			int count = (numbers[6] + numbers[9]) / 2;
-			numbers[6] = count;
-			numbers[9] = count;
-		}
+//		if ((numbers[6] + numbers[9]) % 2 == 0) {
+//			int count = (numbers[6] + numbers[9]) / 2;
+//			numbers[6] = count;
+//			numbers[9] = count;
+//		}else{
+			int c1 = (numbers[6]+numbers[9])/2;
+			int c2 = (numbers[6]+numbers[9])%2;
+			numbers[6] = c1;
+			numbers[9] = c1+c2;
+			
+//		}
 		
 		
 		int max = numbers[0];
